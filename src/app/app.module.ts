4e50e87eece34856ef8e9c36653d79b510b8ea38
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2023-12-22 21:57:53 -0300

    feat: novas páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,12 @@ import { SessaoDepoimentosComponent } from './pages/home/sessao-depoimentos/sess
 import { SessaoPatrocinadoresComponent } from './pages/home/sessao-patrocinadores/sessao-patrocinadores.component';
 import { SessaoComoAjudarComponent } from './pages/home/sessao-como-ajudar/sessao-como-ajudar.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { QuemSomosComponent } from './pages/quem-somos/quem-somos.component';
+import { KarateSemFronteirasComponent } from './pages/karate-sem-fronteiras/karate-sem-fronteiras.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { SejaUmDoadorComponent } from './pages/seja-um-doador/seja-um-doador.component';
+import { TransparenciaComponent } from './pages/transparencia/transparencia.component';
 
 
 @NgModule({
@@ -28,6 +34,12 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao
     SessaoPatrocinadoresComponent,
     SessaoComoAjudarComponent,
     PaginaNaoEncontradaComponent,
+    QuemSomosComponent,
+    KarateSemFronteirasComponent,
+    BlogComponent,
+    ContatoComponent,
+    SejaUmDoadorComponent,
+    TransparenciaComponent,
    
     
   
